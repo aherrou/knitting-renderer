6b779edbdee04e7e54151254de7f701e9d5342cb
Agathe Herrou <herrou@grame.fr>
2024-11-16 22:50:17 +0100

    Makes the stitch positioning relative and a loop to generate a work

--- gen.py
+++ gen.py
@@ -11,13 +11,30 @@ boilerplate_end = '''
 '''
 
 # maille à l'endroit
-knit_stitch = '''<path d="M 0 40 L 40 40 L 40 120" 
-fill="transparent" stroke="black"/>
-<path d="M 120 40 L 80 40 L 80 120"
-fill="transparent" stroke="black"/>
-<path d="M 40 0 L 30 80 L 90 80 L 80 0"
-fill="transparent" stroke="black"/>'''
+# toujours rentre dans un carré 120x120
+def knit_stitch(x0, y0) :
+    path1 = '''<path d="M '''+str(x0)+" "+str(40+y0)+" H "+str(40+x0)+" V "+str(120+y0)+'''" 
+    fill="transparent" stroke="black" stroke-width="5"/>'''
+    path2='''
+    <path d="M '''+ str(120+x0)+" "+str(40+y0)+" H "+str(80+x0)+" V "+str(120+y0)+'''"
+    fill="transparent" stroke="black" stroke-width="5"/>'''
+    path3='''<path d="M '''+str(40+x0)+" "+str(y0)+" L "+str(30+x0)+" "+str(80+y0)+" H "+str(90+x0)+" L "+str(80+x0)+" "+str(y0)+'''"
+    fill="transparent" stroke="black" stroke-width="5"/>'''
+    return (path1 +path2+path3)
 
-outfile.write(boilerplate_begin
-              +knit_stitch
-              +boilerplate_end)
+output = boilerplate_begin
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        output = output+knit_stitch(0+i*120,0+j*120)
+
+output = output+boilerplate_end
+
+outfile.write(output)
+
+# outfile.write(boilerplate_begin
+#               +knit_stitch(0,0)
+#               +knit_stitch(0,120)
+#               +knit_stitch(120,0)
+#               +knit_stitch(120,120)
+#               +boilerplate_end)
