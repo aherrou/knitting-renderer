6007044252dd9cef6baece70ef7779adbb6c58d0
Agathe Herrou <herrou@grame.fr>
2024-11-17 10:20:26 +0100

    Curve the thread + color it with gradient

--- gen.py
+++ gen.py
@@ -1,8 +1,30 @@
 outfile = open(r"/tmp/test.svg", "w")
 
-boilerplate_begin = '''<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'
-         width='100%' height='100%' viewPort='0 0 1000 1000'>
-         <!-- put some content in here -->'''
+boilerplate_begin = '''
+<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'
+width='100%' height='100%' viewPort='0 0 1000 1000'>
+<defs>
+
+<linearGradient id="gradienth1" x1="0%" y1="0%" x2="100%" y2="0%">
+<stop offset="0%"   stop-color="#05a"/>
+<stop offset="100%" stop-color="#0a5"/>
+</linearGradient>
+
+<linearGradient id="gradienth2" x1="100%" y1="0%" x2="0%" y2="0%">
+<stop offset="0%"   stop-color="#05a"/>
+<stop offset="100%" stop-color="#0a5"/>
+</linearGradient>
+
+<linearGradient id="gradientv" x1="0%" y1="100%" x2="0%" y2="0%">
+<stop offset="0%"   stop-color="#05a"/>
+<stop offset="100%" stop-color="#0a5"/>
+</linearGradient>
+
+<style><![CDATA[
+<!-- CSS goes here-->
+]]></style>
+</defs>
+<!-- put some content in here -->'''
 
 red_square = '''<rect x='0' y='0' width='100' height='100' fill='red'></rect>'''
 
@@ -10,17 +32,23 @@ boilerplate_end = '''
          </svg>
 '''
 
+def str_coords(x,y):
+    return str(x)+" "+str(y)
+
 # maille à l'endroit
 # toujours rentre dans un carré 120x120
 def knit_stitch(x0, y0) :
-    path1 = '''<path d="M '''+str(x0)+" "+str(40+y0)+" H "+str(40+x0)+" V "+str(120+y0)+'''" 
-    fill="transparent" stroke="black" stroke-width="5"/>'''
+    path1 = '''<path d="M '''+str_coords(x0, 40+y0)+" Q "+str_coords(40+x0, 40+y0)+" "+str_coords(40+x0, 120+y0)+" L "+str_coords(40+x0, 120+y0)+'''" 
+    fill="transparent" stroke="url(#gradienth1)" stroke-width="5"/>'''
+
     path2='''
-    <path d="M '''+ str(120+x0)+" "+str(40+y0)+" H "+str(80+x0)+" V "+str(120+y0)+'''"
-    fill="transparent" stroke="black" stroke-width="5"/>'''
-    path3='''<path d="M '''+str(40+x0)+" "+str(y0)+" L "+str(30+x0)+" "+str(80+y0)+" H "+str(90+x0)+" L "+str(80+x0)+" "+str(y0)+'''"
-    fill="transparent" stroke="black" stroke-width="5"/>'''
-    return (path1 +path2+path3)
+    <path d="M '''+ str_coords(120+x0, 40+y0)+" Q "+str_coords(80+x0, 40+y0)+" "+str_coords(80+x0, 120+y0)+" L "+str_coords(80+x0, 120+y0)+'''"
+    fill="transparent" stroke="url(#gradienth2)" stroke-width="5"/>'''
+
+    path3='''<path d="M '''+str_coords(40+x0, y0)+" C "+str_coords(x0+40, y0+40)+" "+str_coords(x0, 90+y0)+" "+str_coords(x0+60, 90+y0)+" S "+str_coords(x0+80, y0+40)+" "+str_coords(x0+80, y0)+'''"
+    fill="transparent" stroke="url(#gradientv)" stroke-width="5"/>'''
+
+    return (path1+path2+path3)
 
 output = boilerplate_begin
 
